92fe95cfb7c0b178f66db112a0b2daba8bb37323
mellowmarsach <mellow.marsach@gmail.com>
2026-09-03 22:25:00 -0600

    change message

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import express, { type Express, type Request, type Response } from "express";
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello World! It's me, Aedin :>");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
